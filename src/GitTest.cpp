7d0859ae02cfbc4e7e0a8ab567ed3b2ad86ae8ac
wy <b01482@yeah.net>
2018-02-25 22:29:09 +0800

    Code alignment

--- src/GitTest.cpp
+++ src/GitTest.cpp
@@ -1,28 +1,28 @@
-//============================================================================
-// Name        : GitTest.cpp
-// Author      : good@luck
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+/* ============================================================================ */
+/* Name        : GitTest.cpp */
+/* Author      : good@luck */
+/* Version     : */
+/* Copyright   : Your copyright notice */
+/* Description : Hello World in C++, Ansi-style */
+/* ============================================================================ */
 
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
-	cout << "---------------------\n"
-		 << "      I love you.    \n"
-		 << "---------------------\n"
-		 << endl; // I love you.
+    cout	<< "---------------------\n"
+            << "      I love you.    \n"
+            << "---------------------\n"
+            << endl; /* I love you. */
 
-	/* Add the comments */
+    /* Add the comments */
 
     printf("Add the code comments\n\n");
 
     printf("Add another piece of code.\n");
 
-	return 0;
+    return 0;
 }
-
 
