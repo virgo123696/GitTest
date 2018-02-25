e023d057803cfbef4507d867e09bcd53ed0a5d7e
wy <b01482@yeah.net>
2018-02-25 20:27:54 +0800

    Add the code.

---
+++ src/GitTest.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : GitTest.cpp
+// Author      : good@luck
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "----------------------------\n"
+		 << "         I love you++.      \n"
+	     << "----------------------------\n"
+		 << endl; // prints I love you++.
+
+	return 0;
+}
---
+++ src/Gittest.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : Gittest.c
+ Author      : good@luck
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	printf("----------------------------------------\n");
+	printf("             I love you.                \n");
+	printf("----------------------------------------\n");
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
+
